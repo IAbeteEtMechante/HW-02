2e5ce3cd1155dd5d4aa8b2bfc2c69f4586302b8e
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-11 09:47:02 +0700

    formating adapted to the number of digits of the biggest number in the triangle

--- src/main/java/space/harbour/java/hw2/Pascal.java
+++ src/main/java/space/harbour/java/hw2/Pascal.java
@@ -4,11 +4,20 @@ import java.util.Scanner;
 
 public class Pascal {
 
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return factorial(n - 1) * n;
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of Pascal triangle: ");
         int n = sc.nextInt();
+
 
         int[][] triangle = new int[n][n];
         triangle[0][0] = 1;
@@ -19,14 +28,30 @@ public class Pascal {
             }
         }
         int offset;
+        int previousOffset = -1;
+        int displaySize;
+        int m;
+        m = n / 2;
+        //        displaySize = 
+        //   String.valueOf(factorial(n - 1) / factorial(m) / factorial(n - 1 - m)).length();
+        displaySize = String.valueOf(triangle[n - 1][m]).length();
+
+        //      int biggestElement = factorial(n - 1) / (factorial(m) * factorial(n - 1 - m));
+        //     System.out.println("Biggest element: " + biggestElement);
+        // displaySize = 6;
+        String format = "%" + String.valueOf(displaySize) + "d";
+        System.out.println("displaySize: " + displaySize);
         for (int i = 0; i < n; i++) {
-            offset = n - 1 - i;
+            offset = ((n - 1 - i) * displaySize) / 2;
+            // we want at least a step of one between lines
+            offset = Math.max(offset, previousOffset + 1); 
             for (int k = 0; k < offset; k++) {
                 System.out.print(" ");
             }
 
             for (int j = 0; j <= i; j++) {
-                System.out.print(triangle[i][j]);
+                //System.out.print(triangle[i][j]);
+                System.out.printf(format, triangle[i][j]);
                 System.out.print(" ");
             }
             System.out.println();
